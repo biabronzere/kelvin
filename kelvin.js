c4aa15d4d5bd7454b25bf20bec9dfd58a3bcdf34
biabronzere <127956367+biabronzere@users.noreply.github.com>
2023-08-02 13:38:55 -0300

    Add files via upload

---
+++ kelvin.js
@@ -0,0 +1,20 @@
+/*determinei o valor Kelvin*/
+const kelvin = "293"
+
+/*determinei o valor de celsius usando o valor de kevin*/ 
+const celsius = (kevin-293)
+
+/*transformei celsius em fahrenheit*/
+const fahrenheit = celsius * (9/5)+ 32
+
+/*arredondei o fahrenheit para baixo*/
+fahrenheit = Math.floor(fahrenheit);
+
+/*eu fiz ele imprimir o resultado do fahrenheit na tela*/
+console.log(`A temperatura é ${fahrenheit} graus Fahrenheit`);
+
+/*tranformei celsius em newton e arredondei para baixo*/
+const newton = Math.floor(celsius * (33 / 100));
+
+/*eu fiz ele imprimir o resultado do newton na tela*/
+console.log(`A temperatura é ${newton} graus Newton.`)
